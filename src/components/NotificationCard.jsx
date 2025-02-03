1fe5d9119da468a07887e87ba788fcc67ee91a7b
DanaiWee <Danai_wee@hotmail.com>
2025-02-03 13:05:53 +0700

    Updated notification detail

--- src/components/NotificationCard.jsx
+++ src/components/NotificationCard.jsx
@@ -1,10 +1,11 @@
 
 const NotificationCard = ({item}) => {
-  const {title, date, time} = item;
+  const {title, date, time, description} = item;
   return (
     <div className='w-full flex flex-col bg-gray-500 rounded-lg px-3 py-2 relative mt-2'>
         <p className='font-bold text-md text-gray-900'>{title}</p>
-        <p className='text-sm text-gray-200'>
+        <p className='text-sm text-gray-900'>{description}</p>
+        <p className='text-sm text-gray-200 mt-1'>
             {date}, 
             <span>{time}</span>
         </p>
